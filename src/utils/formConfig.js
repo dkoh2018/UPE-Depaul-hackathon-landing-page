294b2745d0a593531a619569ba69bbdd68549fce
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2026-01-09 01:28:56 -0600

    chore: update DemonHacks icon URL

--- src/utils/formConfig.js
+++ src/utils/formConfig.js
@@ -14,14 +14,14 @@ export const GRAD_YEAR_OPTIONS = ["2026", "2027", "2028", "2029", "2030"];
 
 export const TRACK_OPTIONS = [
     {
-        title: "Smart City and Infrastructure",
+        title: "Smart City & Infrastructure",
         description: "Focus on transportation, energy, traffic, connectivity in the Loop",
-        value: "Smart City and Infrastructure (Focus on transportation, energy, traffic, connectivity in the Loop)"
+        value: "Smart City & Infrastructure (Focus on transportation, energy, traffic, connectivity in the Loop)"
     },
     {
-        title: "Community and Social Impact",
+        title: "Community & Social Impact",
         description: "Focus on accessibility, public safety, local businesses, or homelessness",
-        value: "Community and Social Impact (Focus on accessibility, public safety, local businesses, or homelessness)"
+        value: "Community & Social Impact (Focus on accessibility, public safety, local businesses, or homelessness)"
     }
 ];
 
